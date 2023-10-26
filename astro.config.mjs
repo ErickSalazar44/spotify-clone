d01229ed5c52c0a4111f9b191825bf301f3c0e22
ErickSalazar44 <erickearl22@gmail.com>
2023-10-26 18:52:11 -0500

    prefetch agregado config astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), prefetch()],
   output: "server",
   adapter: vercel()
 });
