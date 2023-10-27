11fa1ea9b28bc6ed72758f77a139ee509ff6cfb7
ErickSalazar44 <erickearl22@gmail.com>
2023-10-26 20:36:07 -0500

    prefetch quitado

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,9 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
 
-import prefetch from "@astrojs/prefetch";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), prefetch()],
-  output: "server",
-  adapter: vercel()
+    integrations: [tailwind(), react()],
+    output: "server",
+    adapter: vercel(),
 });
